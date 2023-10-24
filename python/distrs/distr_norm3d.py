331083de7cebad966f3e04b900c8870bf2bd12f7
ivaquero <msaintms@outlook.com>
2023-10-24 20:23:26 +0800

    build: 🎉 v0.0.1

---
+++ python/distrs/distr_norm3d.py
@@ -0,0 +1,62 @@
+"""
+Original Author: Xavier Yang
+Link: https://github.com/ivaquero
+"""
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+
+xs = np.array([[1.0, 0.0], [2.0, 1.0], [2, 1.0], [5, 1.0]])
+ps = np.array(
+    [
+        [
+            [1.0, 0.5],
+            [0.5, 1.0],
+        ],
+        [
+            [1.0, 0.5],
+            [0.5, 1.0],
+        ],
+        [
+            [1.0, 0.5],
+            [0.5, 1.0],
+        ],
+        [
+            [1.0, 0.5],
+            [0.5, 1.0],
+        ],
+    ]
+)
+
+x_range, y_range = (-5, 25), (-5, 5)
+N = 75
+
+xs = np.asarray(xs)
+x = np.linspace(x_range[0], x_range[1], N)
+y = np.linspace(y_range[0], y_range[1], N)
+xx, yy = np.meshgrid(x, y)
+zv = np.zeros((N, N))
+
+for mean, cov in zip(xs, ps):
+    multinorm = stats.multivariate_normal(mean, cov)
+    zs = np.array(
+        [multinorm.pdf(np.array([i, j])) for i, j in zip(np.ravel(xx), np.ravel(yy))]
+    )
+    zv += zs.reshape(xx.shape)
+
+ax = plt.figure().add_subplot(111, projection="3d")
+ax.plot_surface(
+    xx,
+    yy,
+    zv,
+    rstride=1,
+    cstride=1,
+    lw=0.5,
+    edgecolors="#191919",
+    antialiased=True,
+    shade=True,
+    cmap=cm.autumn,
+)
+ax.view_init(elev=40.0, azim=230)
+plt.show()
